b3c82515968f97efac8aee221777d2ca60a4ab1d
akufanky <128762877+akufanky@users.noreply.github.com>
2023-05-20 08:33:58 +0700

    Menambahkan Stored Procedure
    Menambah Stored Procedure Insert,Update dan Delete pada pasien,obat dan dokter.

--- projectAkhir/db_pa.sql
+++ projectAkhir/db_pa.sql
@@ -53,3 +53,141 @@ CREATE TABLE History (
   aksi VARCHAR(100),
   tanggal TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+DELIMITER //
+CREATE PROCEDURE InsertPasienBaru(
+  IN p_pasien_id INT,
+  IN p_nama_lengkap VARCHAR(100),
+  IN p_alamat VARCHAR(200),
+  IN p_tanggal_lahir DATE,
+  IN p_nomer_hp VARCHAR(20),
+  IN p_gender VARCHAR(10),
+  IN p_riwayat_kesehatan VARCHAR(500),
+  IN p_prosedur_operasi VARCHAR(500),
+  IN p_konsultasi VARCHAR(500),
+  IN p_alergi VARCHAR(500),
+  IN p_informasi_asuransi VARCHAR(200)
+)
+BEGIN
+  INSERT INTO pasien
+  VALUES (p_pasien_id, p_nama_lengkap, p_alamat, p_tanggal_lahir,p_nomer_hp, p_gender, p_riwayat_kesehatan, p_prosedur_operasi,p_konsultasi,p_alergi,p_informasi_asuransi);
+END//
+DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE InsertObatBaru(
+  IN o_obat_id INT,
+  IN o_nama_obat VARCHAR(100),
+  IN o_kandungan VARCHAR(500),
+  IN o_harga VARCHAR(200)
+)
+BEGIN
+  INSERT INTO obat
+  VALUES (o_obat_id, o_nama_obat,o_kandungan,o_harga);
+END//
+DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE InsertDokterBaru(
+  IN d_dokter_id INT,
+  IN d_nama_lengkap VARCHAR(100),
+  IN d_spesialis VARCHAR(100),
+  IN d_nomer_hp VARCHAR(20),
+  IN d_email VARCHAR(100)
+)
+BEGIN
+  INSERT INTO dokter
+  VALUES (d_dokter_id, d_nama_lengkap,d_spesialis,d_nomer_hp,d_email);
+END//
+DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE HapusPasien(
+  IN p_pasien_id INT
+)
+BEGIN
+  DELETE FROM pasien WHERE pasien_id = p_pasien_id;
+END //
+DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE HapusObat(
+  IN o_obat_id INT
+)
+BEGIN
+  DELETE FROM obat WHERE obat_id = o_obat_id;
+END //
+DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE HapusDokter(
+  IN d_dokter_id INT
+)
+BEGIN
+  DELETE FROM dokter WHERE dokter_id = d_dokter_id;
+END //
+DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE UpdatePasien(
+  IN p_pasien_id INT,
+  IN p_nama_lengkap VARCHAR(100),
+  IN p_alamat VARCHAR(200),
+  IN p_tanggal_lahir DATE,
+  IN p_nomer_hp VARCHAR(20),
+  IN p_gender VARCHAR(10),
+  IN p_riwayat_kesehatan VARCHAR(500),
+  IN p_prosedur_operasi VARCHAR(500),
+  IN p_konsultasi VARCHAR(500),
+  IN p_alergi VARCHAR(500),
+  IN p_informasi_asuransi VARCHAR(200)
+)
+BEGIN
+  UPDATE pasien
+  SET nama_lengkap = p_nama_lengkap,
+      alamat = p_alamat,
+      tanggal_lahir = p_tanggal_lahir,
+      nomer_hp = p_nomer_hp,
+      gender = p_gender,
+      riwayat_kesehatan = p_riwayat_kesehatan,
+      prosedur_operasi = p_prosedur_operasi,
+      konsultasi = p_konsultasi,
+      alergi = p_alergi,
+	  informasi_asuransi = p_informasi_asuransi
+  WHERE pasien_id = p_pasien_id;
+END //
+DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE UpdateObat(
+  IN o_obat_id INT,
+  IN o_nama_obat VARCHAR(100),
+  IN o_kandungan VARCHAR(500),
+  IN o_harga VARCHAR(200)
+)
+BEGIN
+  UPDATE obat
+  SET nama_obat = o_nama_obat,
+	  kandungan = o_kandungan,
+      harga = o_harga
+  WHERE obat_id = o_obat_id;
+END//
+DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE UpdateDokter(
+  IN d_dokter_id INT,
+  IN d_nama_lengkap VARCHAR(100),
+  IN d_spesialis VARCHAR(100),
+  IN d_nomer_hp VARCHAR(20),
+  IN d_email VARCHAR(100)
+)
+BEGIN
+  UPDATE dokter
+  SET nama_lengkap = d_nama_lengkap,
+      spesialis = d_spesialis,
+      nomer_hp = d_nomer_hp,
+      email = d_email
+  WHERE dokter_id = d_dokter_id;
+END//
+DELIMITER ;
